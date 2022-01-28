c4de461dce3492c855f533099ca2c6d862215ce2
Snerksss <berndts.tin21@student.dhbw-heidenheim.de>
2022-01-28 11:40:56 +0100

    rotation of picture

--- src/output_framework/output_framework.py
+++ src/output_framework/output_framework.py
@@ -6,7 +6,7 @@ from PIL import Image, ImageDraw, ImageFont
 class OutputFramework:
 
     @staticmethod
-    def setWindow(ausgabe: list):
+    def setWindow(ausgabe: list, rotation = 90):
         """
         Zeigt eine Ausgabe an.
         :param ausgabe: Liste mit Dimensionen (16,16,3) (r (0 - 255) , g (0 - 255), b (0 - 255))
@@ -14,10 +14,12 @@ class OutputFramework:
         for x in range(len(ausgabe)):
             for y in range(len(ausgabe[x])):
                 uni.set_pixel(x, y, ausgabe[x][y][0], ausgabe[x][y][1], ausgabe[x][y][2])
+
+        uni.set_rotation(rotation)
         uni.show()
 
     @staticmethod
-    def setPixel(x: int, y: int, r: int, g: int, b: int):
+    def setPixel(x: int, y: int, r: int, g: int, b: int, rotation = 90):
         """
         Setzt einzelne Pixel, werden aber erst mit .show angezeigt
         :param x: x Position des Pixels
@@ -26,10 +28,11 @@ class OutputFramework:
         :param g: gruene Farbe des Pixels
         :param b: blaue Farbe des Pixels
         """
+        uni.set_rotation(rotation)
         uni.set_pixel(x, y, r, g, b)
 
     @staticmethod
-    def showText(text, ro, gr, bl, fontSize, speed, minimum):
+    def showText(text, ro, gr, bl, fontSize, speed, minimum, rotation = 90):
         width, height = uni.get_shape()
         colour = (ro, gr, bl)
         font_file = '/usr/share/fonts/truetype/freefont/FreeSans.ttf'
@@ -46,7 +49,7 @@ class OutputFramework:
         image = Image.new('RGB', (textWidth, textHeight), (0, 0, 0))
         draw = ImageDraw.Draw(image)
         draw.text((textX, textY), text, colour, font=font)
-        uni.rotation(90)
+        uni.set_rotation(rotation)
         for scroll in range(textWidth - width):
             for x in range(width):
                 for y in range(height):
@@ -59,6 +62,7 @@ class OutputFramework:
         uni.off()
 
     @staticmethod
-    def show():
+    def show(rotation = 90):
+        uni.set_rotation(rotation)
         uni.show()
 
